1a8fbdd4747d70de264bf80674f1ca3be2b559a7
Kirill <paltsev.kir@gmail.com>
2023-06-07 13:53:24 +0400

    remove unnecessary const

--- core/block_number.go
+++ core/block_number.go
@@ -5,9 +5,7 @@ import "encoding/binary"
 type BlockNumber uint64
 
 func (b BlockNumber) ToBytes() []byte {
-	const lenOfByteSlice = 8
-
-	numBytes := make([]byte, lenOfByteSlice)
+	numBytes := make([]byte, 8)
 	binary.BigEndian.PutUint64(numBytes, uint64(b))
 
 	return numBytes
