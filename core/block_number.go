a4fff1fba00b2fab0d1731267aa6d20f4169143c
Kirill <paltsev.kir@gmail.com>
2023-06-07 14:08:45 +0400

    Revert "remove unnecessary const"
    This reverts commit 1a8fbdd4747d70de264bf80674f1ca3be2b559a7.

--- core/block_number.go
+++ core/block_number.go
@@ -5,7 +5,9 @@ import "encoding/binary"
 type BlockNumber uint64
 
 func (b BlockNumber) ToBytes() []byte {
-	numBytes := make([]byte, 8)
+	const lenOfByteSlice = 8
+
+	numBytes := make([]byte, lenOfByteSlice)
 	binary.BigEndian.PutUint64(numBytes, uint64(b))
 
 	return numBytes
